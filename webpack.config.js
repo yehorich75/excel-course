1d8f0d514d9808b05fde4b087b4e6e2fc1307bfc
Yegorich75 <yegorich75@gmail.com>
2020-06-03 09:45:43 +0300

    eslint error fix

--- webpack.config.js
+++ webpack.config.js
@@ -89,12 +89,12 @@ module.exports = {
 				test: /\.js$/,
 				exclude: /node_modules/,
 				use: jsLoaders(),
-				loader: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env']
-					}
-				},
+				// loader: {
+				// 	loader: 'babel-loader',
+				// 	options: {
+				// 		presets: ['@babel/preset-env']
+				// 	}
+				// },
 			},
 		],
 	},
