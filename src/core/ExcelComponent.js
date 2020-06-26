cf5067b0850c2e715af413448d5959b6b7991e42
yegorich75 <yegorich75@gmail.com>
2020-06-26 06:51:16 +0300

    add child components home

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,5 +1,8 @@
-import { DomListener } from "@core/DomListener"
+import { DomListener } from '@core/DomListener';
 
 export class ExcelComponent extends DomListener {
-    
+  // Возвращает шаблон компонента
+  toHTML() {
+    return '';
+  }
 }
